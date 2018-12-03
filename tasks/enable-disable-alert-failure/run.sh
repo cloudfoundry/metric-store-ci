926d8aa0e170e49783d486331f3e3ed79e8633a5
Mark Gangl <mgangl@pivotal.io>
2018-12-03 15:49:10 -0700

    send an alert for every 5th cf-deploy failure to the main slack channel
    Signed-off-by: Jesse Weaver <jeweaver@pivotal.io>

--- tasks/enable-disable-alert-failure/run.sh
+++ tasks/enable-disable-alert-failure/run.sh
@@ -6,9 +6,13 @@ set -eoux pipefail
 current_version=$(cat slack-rate-limit-version/version)
 patch_version=$(patch_from_semver ${current_version})
 alert_multiple=${alert_multiple:-5}
+ignore_first=${ignore_first:-false}
 
 if [[ ${patch_version} -gt 1 && $(( ${patch_version} % ${alert_multiple} )) -ne 0 ]]; then
   echo "Not the first failure or a multiple of ${alert_multiple}, disable alert (failure #${patch_version})"
+  alert_disable
+elif [[ ${patch_version} -eq 1 ]] && $ignore_first; then
+  echo "Ignore first failure, disable alert (failure #${patch_version})"
   alert_disable
 else
   echo "First failure or a multiple of ${alert_multiple}, enable alert (failure #${patch_version})"
