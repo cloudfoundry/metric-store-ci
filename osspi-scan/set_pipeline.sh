58c661e40479e27b481e306afba9ea3178d647d0
Jeanette Booher <jbooher@vmware.com>
2023-05-24 09:44:25 -0600

    add tile osspi scan

---
+++ osspi-scan/set_pipeline.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -efuo pipefail
+
+which fly || (
+  echo "This requires fly to be installed"
+  echo "Download the binary from https://github.com/concourse/concourse/releases or from the Runway Concourse: https://runway-ci.eng.vmware.com"
+  exit 1
+)
+
+pipeline_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+OSM_ENVIRONMENT=${OSM_ENVIRONMENT:-"production"}
+echo "using OSM_ENVIRONMENT: ${OSM_ENVIRONMENT}. Valid environments are beta and production"
+
+fly --target runway-ci set-pipeline \
+    --pipeline tile-scan-metric-store \
+    --config "${pipeline_dir}/pipeline.yml" \
+    --var osm-environment="${OSM_ENVIRONMENT}"
