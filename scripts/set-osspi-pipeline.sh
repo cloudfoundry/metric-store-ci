381e3ea5d64ade887ea8b8ec41fd98f96419bc26
jbooherl <jeanette.booher@broadcom.com>
2024-04-30 09:47:58 -0600

    bump version and update pipeline

--- scripts/set-osspi-pipeline.sh
+++ scripts/set-osspi-pipeline.sh
@@ -3,30 +3,28 @@ set -efuo pipefail
 
 which fly || (
   echo "This requires fly to be installed"
-  echo "Download the binary from https://github.com/concourse/concourse/releases or from the Runway Concourse: https://runway-ci.eng.vmware.com"
+  echo "Download the binary from https://github.com/concourse/concourse/releases or from the Runway Concourse: https://runway-ci-sfo.eng.vmware.com"
   exit 1
 )
 
-fly -t runway sync || (
-  echo "This requires the runway target to be set"
-  echo "Create this target by running 'fly -t runway login -c https://runway-ci.eng.vmware.com -n tobs-k8s-group'"
+fly -t runway-ms sync || (
+  echo "This requires the runway-ms target to be set"
+  echo "Create this target by running 'fly -t runway-ms login -c https://runway-ci-sfo.eng.vmware.com -n phoenix-metric-store'"
   exit 1
 )
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 config_path=${ROOT_DIR}/pipelines/osspi-metric-store.yml
 
-TEAM="${TEAM:-tobs-k8s-group}"
 pipeline_name=${PIPELINE_NAME:-osspi-metric-store}
-VERSION=${VERSION:-"1.6.0"}
+VERSION=${VERSION:-"1.7.0"}
 OSM_ENVIRONMENT=${OSM_ENVIRONMENT:-"production"}
 
 echo "using OSM_ENVIRONMENT: ${OSM_ENVIRONMENT}. Valid environments are beta and production"
 
-fly --target runway set-pipeline \
+fly --target runway-ms set-pipeline \
     --pipeline ${pipeline_name} \
     --config ${config_path} \
     --var tile-version="${VERSION}" \
     --var osm-environment="${OSM_ENVIRONMENT}"
 
-# fly --target runway login --concourse-url https://runway-ci.eng.vmware.com --team-name tobs-k8s-group
